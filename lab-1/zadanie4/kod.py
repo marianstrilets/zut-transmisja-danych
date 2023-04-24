3f2ccfe5b3d7eb226f56ec6a0c2565663a24d5a5
marianstrilets <marianstrilets@gmail.com>
2023-04-25 00:40:49 +0200

    lab-1: Reprezentacja sydnałow w dziedzinie czasu

---
+++ lab-1/zadanie4/kod.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def b(t):
+    Hk = [2, 6, 10]
+    values = []
+    for h in Hk:
+        values.append(np.sin( h * t * np.pi) / (2 + np.cos(h**2 * np.pi * t)))
+    return values
+        
+    
+    
+#------------------------------------- Zadanie 4 --------------------------------------------------------------------------------------
+# Wygenerować i wykreslić sygnały bk(t) (k = 1,2,3) dla  fs = 22.05 kHz oraz Tc = 1s
+#--------------------------------------------------------------------------------------------------------------------------------------
+
+def main():
+    # parametry próbkowania
+    Tc = 1                  # [s] czas trwania sygnału, (czas próbkowania w sekundach)
+    fs = 8000               # [Hz] częstotliwosć probkowania
+    N = int(Tc * fs)        # liczba próbek przypadających na cały sygnal
+    Ts = 1 / fs             # okres próbkowania (powina być co najmniej dwa razy więksa niż górna granica częstotliwości sygnalu próbkowanego fmax)
+    t = np.arange(N) * Ts   # czas próbkowania
+    
+    value = b(t)
+    cnt = 1
+    for i in value: 
+        plt.plot(t, i)
+        plt.xlabel('Czas [s]')
+        plt.ylabel('Aplituda')
+        plt.title('Wykres sygnalu b' + str(cnt)  + '(t)')
+        plt.savefig('./b' + str(cnt) + '.png')
+        cnt = cnt+1
+        plt.show()
+
+        
+
+if __name__ == "__main__":
+    main()
